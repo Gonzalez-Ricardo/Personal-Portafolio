ae65d68c5a3097aac79df3806837f04954434010
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2025-01-29 08:55:17 -0600

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+    ignoreDuringBuilds: true,
+},
+    output: "export",
+};
+
 
 export default nextConfig;
+
+
