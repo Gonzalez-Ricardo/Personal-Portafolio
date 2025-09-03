95f7aba55e5a3bc84839b7a1eae35216ea74b420
Sierra117 <ricardogonzalezmendez117@hotmail,com>
2025-09-03 11:39:04 -0600

    actualización 14

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,10 +5,8 @@ import Projects from '@/components/component/Projects'
 import Footer from '@/components/component/footer'
 import Skills from '@/components/component/skills'
 import Experience from '@/components/component/experience'
-import { Loader } from "@/components/component/loader";
 import { GlobeDemo } from "@/components/component/world";
 import React from "react";
-import { Import } from 'lucide-react';
 import { StarsBackground } from '@/components/ui/stars-background';
 
 export default function Home() {
@@ -16,7 +14,6 @@ export default function Home() {
     <div  className="relative">
       {/* StarsBackground como fondo global */}
       <StarsBackground className="fixed top-0 left-0 w-full h-full z-0" />
-      
       {/* Contenido principal */}
       <div className="relative z-10 mx-auto">
         <Header />
